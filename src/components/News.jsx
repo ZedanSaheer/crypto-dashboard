b060450ac4e6ae12a095c609e6073e51951835e6
Zedan Saheer <zed.saheer5@gmail.com>
2021-10-20 22:30:40 +0530

    finished app , fixing small style faults

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -24,7 +24,7 @@ const News = ({ simplified }) => {
                     <Select
                         showSearch
                         className="select-news"
-                        placeholder="Select a crypto"
+                        placeholder="Select a Crypto"
                         optionFilterProp="children"
                         onChange={(value) => setNewsCat(value)}
                         filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) > 0}
@@ -43,7 +43,7 @@ const News = ({ simplified }) => {
                             <div className="news-image-container">
                                 <Title className="news-title" level={5}>{news.name}
                                 </Title>
-                                <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt={news.name} style={{ maxWidth: "200px", maxHeight: "100px" }} />
+                                <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt={news.name} style={{ maxWidth: "100px", maxHeight: "200px" }} />
                             </div>
                             <p>
                                 {news.description > 100 ? `${news.description.substring(0, 100)}...` : news.description}
